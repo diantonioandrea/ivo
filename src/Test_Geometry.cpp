14ea41b13b34c08a7e3878725a3851238ea5d6c2
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-07-23 21:03:16 +0200

    Update Test_Geometry.cpp

--- src/Test_Geometry.cpp
+++ src/Test_Geometry.cpp
@@ -14,8 +14,10 @@ int main(int argc, char **argv) {
 
     // Points.
     ivo::Point21 a{0, 0, 0};
-    ivo::Point21 b{0, 1, 0};
+    ivo::Point21 b{1, 0, 0};
     ivo::Point21 c{1, 1, 0};
+
+    ivo::Point21 d{0, 1, 0};
 
     // Edges.
     ivo::Edge21 ab{a, b};
@@ -27,6 +29,8 @@ int main(int argc, char **argv) {
     ivo::Line21 bcl{b, c};
     ivo::Line21 cal{c, a};
 
+    ivo::Line21 cdl{c, d};
+
     // Polygon.
     ivo::Polygon21 abc{{a, b, c}};
 
@@ -34,13 +38,28 @@ int main(int argc, char **argv) {
     std::cout << a << std::endl << std::endl;
     std::cout << b << std::endl << std::endl;
     std::cout << c << std::endl << std::endl;
+
     std::cout << ab << std::endl << std::endl;
     std::cout << bc << std::endl << std::endl;
     std::cout << ca << std::endl << std::endl;
+
     std::cout << abl << std::endl << std::endl;
     std::cout << bcl << std::endl << std::endl;
     std::cout << cal << std::endl << std::endl;
-    std::cout << abc << std::endl;
+
+    std::cout << abc << std::endl << std::endl;
+
+    std::cout << distance(a, b) << std::endl << std::endl;
+    std::cout << distance(b, c) << std::endl << std::endl;
+    std::cout << distance(c, a) << std::endl << std::endl;
+
+    std::cout << distance(abl, c) << std::endl << std::endl;
+    std::cout << distance(bcl, a) << std::endl << std::endl;
+    std::cout << distance(cal, b) << std::endl << std::endl;
+
+    std::cout << distance(abl, bcl) << std::endl << std::endl;
+    std::cout << distance(abl, cal) << std::endl << std::endl;
+    std::cout << distance(abl, cdl) << std::endl;
 
     return 0;
 }
