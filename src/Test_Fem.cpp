7088164c86a9983fcb8feba7631872f843797cb1
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-08-05 15:21:24 +0200

    Tested GL nodes

---
+++ src/Test_Fem.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Test_Fem.cpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief Simple FEM testing.
+ * @date 2024-08-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <Ivo.hpp>
+
+int main(int argc, char **argv) {
+
+    // Gauss-Legendre nodes.
+    auto [nodes, weights] = ivo::gauss1(5, -1.0L, 1.0L);
+
+    std::cout << nodes << std::endl;
+    std::cout << weights << std::endl;
+
+    return 0;
+}
