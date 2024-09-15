cfca64c92d640b82755cb03be280cbf2c12ab732
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-09-15 15:21:26 +0200

    Adder dt u integral

--- src/Problem_Stiffness.cpp
+++ src/Problem_Stiffness.cpp
@@ -26,6 +26,7 @@ namespace ivo {
         auto [nodes2x, nodes2y, weights2] = quadrature2(constants::quadrature);
 
         // Stiffness submatrices.
+        Sparse<Real> T{mesh.dofs(), mesh.dofs()}; // Volume integrals, time.
         Sparse<Real> V{mesh.dofs(), mesh.dofs()}; // Volume integrals.
         Sparse<Real> I{mesh.dofs(), mesh.dofs()}; // Edge integrals.
 
@@ -48,6 +49,9 @@ namespace ivo {
             // VOLUME INTEGRALS - PRECOMPUTING.
 
             // Submatrices.
+            Matrix<Real> T_xy{dofs_xy, dofs_xy};
+            Matrix<Real> T_t{dofs_xy, dofs_xy};
+
             Matrix<Real> V_a_xy{dofs_xy, dofs_xy};
             Matrix<Real> V_a_t{dofs_t, dofs_t};
 
@@ -76,6 +80,11 @@ namespace ivo {
 
             // VOLUME INTEGRALS - COMPUTING.
 
+            // (*', *).
+
+            T_xy = internal::c_scale(weights2_j, phi_s).transpose() * phi_s;
+            T_t = internal::c_scale(weights1_j * diffusion, gradt_phi_t).transpose() * phi_t;
+
             // a(*, *), diffusion.
 
             V_a_xy = internal::c_scale(weights2_j, gradx_phi_s).transpose() * gradx_phi_s + internal::c_scale(weights2_j, grady_phi_s).transpose() * grady_phi_s;
@@ -95,6 +104,7 @@ namespace ivo {
 
             // VOLUME INTEGRALS - BUILDING.
 
+            T(dofs_j, dofs_j, T(dofs_j, dofs_j) + kronecker(T_t, T_xy));
             V(dofs_j, dofs_j, V(dofs_j, dofs_j) + kronecker(V_a_t, V_a_xy));
             V(dofs_j, dofs_j, V(dofs_j, dofs_j) + kronecker(V_bx_t, V_bx_xy) + kronecker(V_by_t, V_by_xy));
             V(dofs_j, dofs_j, V(dofs_j, dofs_j) + kronecker(V_c_t, V_c_xy));
