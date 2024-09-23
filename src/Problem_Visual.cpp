acfd59a1e3b4843d171b4067c6a9fc627ce469f8
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-09-23 16:37:08 +0200

    Update Problem_Visual.cpp

--- src/Problem_Visual.cpp
+++ src/Problem_Visual.cpp
@@ -12,57 +12,68 @@
 
 namespace ivo {
 
-    // /**
-    //  * @brief Solution visualization.
-    //  * 
-    //  * @param mesh Mesh.
-    //  * @param solution Numerical solution.
-    //  * @param filename Output file.
-    //  */
-    // void visual(const Mesh21 &mesh, const Vector<Real> &solution, const std::string &filename) {
+    /**
+     * @brief Solution visualization.
+     * 
+     * @param mesh Mesh.
+     * @param solution Numerical solution.
+     * @param filename Output file.
+     */
+    void visual(const Mesh21 &mesh, const Vector<Real> &solution, const std::string &filename) {
 
-    //     // Quadrature.
-    //     auto [nodes1t, weights1] = quadrature1(constants::quadrature);
-    //     auto [nodes2x, nodes2y, weights2] = quadrature2(constants::quadrature);
+        // Quadrature.
+        auto [nodes1t, weights1] = quadrature1(constants::quadrature);
+        auto [nodes2x, nodes2y, weights2] = quadrature2(constants::quadrature);
 
-    //     // Output.
-    //     std::ofstream output(filename);
+        // Output.
+        std::ofstream output(filename);
 
-    //     // Loop over elements.
-    //     for(Natural j = 0; j < mesh.space() * mesh.time(); ++j) {
+        // Loop over elements.
+        for(Natural j = 0; j < mesh.space() * mesh.time(); ++j) {
 
-    //         // Element.
-    //         Element21 element = mesh.element(j);
+            // Element.
+            Element21 element = mesh.element(j);
 
-    //         // Dofs.
-    //         std::vector<Natural> dofs_j = mesh.dofs(j);
+            // Neighbours.
+            Neighbour21 neighbourhood = mesh.neighbour(j);
 
-    //         // Nodes and basis.
-    //         auto [nodes1t_j, dt_j] = internal::reference_to_element(mesh, j, nodes1t);
-    //         auto [nodes2xy_j, dxy_j] = internal::reference_to_element(mesh, j, {nodes2x, nodes2y});
-    //         auto [nodes2x_j, nodes2y_j] = nodes2xy_j;
+            std::vector<std::array<Integer, 2>> facing = neighbourhood.facing();
+            Natural neighbours = facing.size();
 
-    //         auto [phi_s, gradx_phi_s, grady_phi_s] = basis_s(mesh, j, nodes2xy_j);
-    //         auto [phi_t, gradt_phi_t] = basis_t(mesh, j, nodes1t);
+            // Dofs.
+            std::vector<Natural> dofs_j = mesh.dofs(j);
 
-    //         // Full basis.
-    //         Matrix<Real> phi = kronecker(phi_t, phi_s);
+            // Nodes and basis.
+            auto [nodes1t_j, dt_j] = internal::reference_to_element(mesh, j, nodes1t);
+            auto [phi_t, gradt_phi_t] = basis_t(mesh, j, nodes1t);
 
-    //         // Full nodes.
-    //         std::vector<std::array<Real, 3>> nodes;
+            for(Natural k = 0; k < neighbours; ++k) { // Sub-triangulation.
 
-    //         for(Natural k = 0; k < nodes1t_j.size(); ++k)
-    //             for(Natural h = 0; h < nodes2x_j.size(); ++h)
-    //                 nodes.emplace_back(std::array<Real, 3>{nodes2x_j[h], nodes2y_j[h], nodes1t_j[k]});
+                // Nodes and basis.
+                auto [nodes2xy_j, dxy_j] = internal::reference_to_element(mesh, j, k, {nodes2x, nodes2y});
+                auto [phi_s, gradx_phi_s, grady_phi_s] = basis_s(mesh, j, nodes2xy_j);
 
-    //         // Evaluation.
-    //         Vector<Real> evaluation = phi * solution(dofs_j);
+                auto [nodes2x_j, nodes2y_j] = nodes2xy_j;
 
-    //         // Output.
-    //         for(Natural j = 0; j < nodes.size(); ++j) {
-    //             output << nodes[j][0] << "," << nodes[j][1] << "," << nodes[j][2] << "," << evaluation(j) << std::endl;
-    //         }
-    //     }
-    // }
+                // Full basis.
+                Matrix<Real> phi = kronecker(phi_t, phi_s);
+
+                // Full nodes.
+                std::vector<std::array<Real, 3>> nodes;
+
+                for(Natural l = 0; l < nodes1t_j.size(); ++l)
+                    for(Natural h = 0; h < nodes2x_j.size(); ++h)
+                        nodes.emplace_back(std::array<Real, 3>{nodes2x_j[h], nodes2y_j[h], nodes1t_j[l]});
+
+                // Evaluation.
+                Vector<Real> evaluation = phi * solution(dofs_j);
+
+                // Output.
+                for(Natural j = 0; j < nodes.size(); ++j) {
+                    output << nodes[j][0] << "," << nodes[j][1] << "," << nodes[j][2] << "," << evaluation(j) << std::endl;
+                }
+            }
+        }
+    }
 
 }
