e20cd39a9f8237838823373cb7164b9b6b21b3b5
Andrea Di Antonio <diantonioandrea@icloud.com>
2025-02-19 11:28:00 +0100

    New tests

---
+++ src/Test_tSaturation.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file Test_hSaturation.cpp
+ * @author Andrea Di Antonio (github.com/diantonioandrea)
+ * @brief t saturation test.
+ * @date 2025-02-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "./include/Square.hpp"
+
+int main(int argc, char **argv) {
+
+    if(argc != 3) {
+        std::cout << "Usage: " << argv[0] << " SPACE_DEGREE [p] TIME_DEGREE [q]." << std::endl;
+        return -1;
+    }
+
+    // Degrees.
+    const ivo::Natural p = static_cast<ivo::Natural>(std::atoi(argv[1]));
+    const ivo::Natural q = static_cast<ivo::Natural>(std::atoi(argv[2]));
+
+    assert(p > 0);
+    assert(q > 0);
+
+    // Output.
+    std::ofstream output{"output/tSaturation_" + std::to_string(p) + "_" + std::to_string(q) + ".e21"};
+
+    #ifndef NVERBOSE
+    std::cout << "[Ivo] TEST, Testing time saturation\n" << std::endl;
+    #else
+    std::cout << "[Ivo] TEST, Testing time saturation" << std::endl;
+    #endif
+
+    // Equation.
+    const ivo::Equation equation{ivo::square::convection, ivo::square::diffusion, ivo::square::reaction};
+    const ivo::Initial initial{ivo::square::u0};
+    const ivo::Data data{ivo::square::g, ivo::square::gd, ivo::square::gn};
+
+    // Tests.
+    const ivo::Natural tests = 16;
+
+    // Time elements.
+    const ivo::Natural Nt = 4;
+
+    // Space.
+    const std::vector<ivo::Polygon21> space = ivo::mesher2("data/square/Square_250.p2");
+
+    // Main loops.
+    for(ivo::Natural j = 0; j < tests; ++j) {
+
+        // Timer.
+        auto start = std::chrono::high_resolution_clock::now();
+
+        // Time.
+        const std::vector<ivo::Real> time = ivo::mesher1(0.0L, 1.0L, Nt + j * 2);
+
+        // Mesh.
+        const ivo::Mesh21 mesh{space, time, p, q};
+
+        // Matrix and vector.
+        const ivo::Sparse<ivo::Real> A = ivo::stiffness(mesh, equation);
+        const ivo::Vector<ivo::Real> b = ivo::forcing(mesh, equation, data);
+
+        // Solution.
+        const ivo::Vector<ivo::Real> x = ivo::solve(mesh, A, b, initial);
+
+        // Error.
+        const ivo::Error error{mesh, equation, x, ivo::square::u, ivo::square::u_xy};
+
+        // Elapsed time.
+        const auto timer = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start);
+        const ivo::Real elapsed = timer.count() / 1.0E3L;
+
+        // Output.
+        output << error << "\n" << std::endl;
+
+        #ifndef NVERBOSE
+        std::cout << "\n\t[TEST] Progress: " << j + 1 << "/" << tests << ", " << elapsed << "s\n" << std::endl;
+        #else
+        std::cout << "\t[TEST] Progress: " << j + 1 << "/" << tests << ", " << elapsed << "s" << std::endl;
+        #endif
+    }
+
+    std::cout << "\t[TEST] Exited" << std::endl;
+
+    return 0;
+}
